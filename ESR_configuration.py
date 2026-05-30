10b750a2484c4240c426f63810d754b8603ec757
s5vc1q8t <142373025+s5vc1q8t@users.noreply.github.com>
2026-05-30 13:44:37 +0400

    Initial commit

---
+++ ESR_configuration.py
@@ -0,0 +1,23 @@
+from netmiko.eltex.eltex_esr_ssh import EltexEsrSSH
+
+divice_template = {'device_type' : 'eltex_esr',
+          'ip': '172.16.21.61',
+          'username' : 'admin',
+          'password' : '123',
+          }
+
+ssh = EltexEsrSSH(**divice_template)
+
+
+
+commands = ['username user101',
+            'privilege 15',
+            'password password']
+
+ssh.send_config_set(commands)
+
+ssh.commit()
+
+ssh.send_command('confirm')
+
+ssh.disconnect()
