0a84cf39884882333694eb3b7a501536ac91ca59
s5vc1q8t <142373025+s5vc1q8t@users.noreply.github.com>
2026-05-31 17:02:47 +0400

    Update Mikrotik_Backup.py

--- Mikrotik/Mikrotik_Backup.py
+++ Mikrotik/Mikrotik_Backup.py
@@ -2,6 +2,7 @@ from netmiko.mikrotik.mikrotik_ssh import MikrotikRouterOsSSH
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from yaml import safe_load
+from os import makedirs
 
 with open('inventory.yaml', 'r', encoding='utf-8') as inventory:
     config = safe_load(inventory)
@@ -17,20 +18,33 @@ NODE = {
     'password': PASSWORD
 }
 
+ERROR_LOG_FILE = "./backup_errors.txt"
+
 def backup(ip):
-    device = NODE.copy()
-    device['host'] = ip
 
-    connection = MikrotikRouterOsSSH(**device)
+    identity = None
 
-    identity = connection.send_command('/system identity print').split()[1]
-    output_csr = connection.send_command('/export show-sensitive')
+    try:
+        device = NODE.copy()
+        device['host'] = ip
 
-    filename = f"{identity}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.rsc"
-    with open(filename, 'w', encoding='utf-8') as file:
-        file.write(output_csr)
+        connection = MikrotikRouterOsSSH(**device)
+        identity = connection.send_command('/system identity print').split()[1]
 
-    connection.disconnect()
+        output_csr = connection.send_command('/export show-sensitive')
+
+        makedirs(f"./{identity}", exist_ok=True)
+
+        filename = f"./{identity}/{identity}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.rsc"
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(output_csr)
+
+        connection.disconnect()
+
+    except Exception as e:
+        error_msg = str(e).split('\n')[0]
+        with open(ERROR_LOG_FILE, 'a', encoding='utf-8') as f:
+            f.write(f"{datetime.now()} | IP: {ip} | Устройство: {identity} | Ошибка: {error_msg}\n")
 
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as ex:
     ex.map(backup, IPS)
